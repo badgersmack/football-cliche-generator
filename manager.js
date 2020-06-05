281ed4d15f990b3f73d7233dbb8d33a95d4d1655
Luke <48770392+badgersmack@users.noreply.github.com>
2020-06-05 10:42:00 +0100

    Working demo
    initial attempt included express and ejs which i forgot to delete before adding files

---
+++ manager.js
@@ -0,0 +1,50 @@
+const cliche1 = [
+    "I didn’t see the incident. ",
+    "At the end of the day ",
+    "The referee needs to take a long, hard look at himself. ",
+    "I accept responsibility for the loss. ",
+    "There was a lot of honesty in that dressing room. ",
+    "I think the better team lost. ",
+    "I think we edged it. ",
+    "I’m sorry, I’m not having that. ",
+    "I prefer to focus on my team. ",
+    "I couldn’t be prouder of the lads today. ",
+    "He’ll learn from that. ",
+    "The referee seemed determined to be the centre of attention today. ",
+    "To be fair "
+]
+
+const cliche2 = [
+    "I thought the support we got was first-class today. ",
+    "There are no easy games in this league. ",
+    "We have to play how we’re capable of playing. ",
+    "The game was really a lot closer than the final score would suggest. ",
+    "It was a game of two halves. ",
+    "It's a six pointer. ",
+    "It comes with the territory. ",
+    "I’m going to get myself in trouble. ",
+    "I don't want to say too much, because I'll get into trouble, but ",
+    "I don't care if I get into trouble, ",
+    "Football is a results business. ",
+    "Ultimately, I will be judged on results. "
+]
+
+const cliche3 = [
+    "We’re just focused on the next game.",
+    "He's not that sort of player.",
+    "We never seem get the big decisions.",
+    "We never seem get the rub of the green.",
+    "Those decisions even themselves out over the course of a season.",
+    "The important thing is we got the three points.",
+    "All I'm interested in is preparing for the next game on Tuesday.",
+    "I've never walked away from anything in my life, and I'm not going to start now.",
+    "Both myself and the board want what's best for this football club.",
+    "The fans were magnificent today. They deserve better.",
+    "The fans pay their money and they have every right to make their feelings known."
+]
+
+document.getElementById("button").addEventListener("click", randomCliche);
+function randomCliche() {document.getElementById("result").innerHTML = cliche1[Math.floor(Math.random()*cliche1.length)] +
+    cliche2[Math.floor(Math.random()*cliche2.length)] +
+    cliche3[Math.floor(Math.random()*cliche3.length)]};
+
---
+++ pundit.js
@@ -0,0 +1,46 @@
+13 He's lost the dressing room
+14 He fell into the offside trap
+15 He's got genuine pace
+16 Away goals count double
+17 It could be a cricket score
+18 A good time to score
+19 A team that likes to play football
+20 In his locker
+21 This game needs a goal
+22 You can't win the title in August but you can lose it
+23 He was in acres of space
+
+24 A no-nonsense player
+25 He'll have nightmares about that for years
+26 That was a great cross but there was no one there
+28 Some tired legs out there
+29 A bit of 'handbags' going on there
+30 Beckham territory
+not for me clive
+3 Goals wins games
+4 They scored too early
+5 He certainly knows where the goal is
+6 If that was on target, it would have been a goal
+7 Schoolboy defending
+8 Good squad on paper
+9 Yard of pace
+10 Your Rooneys/Lampards/whoevers
+11 Cultured left foot
+12 He gave 110%
+
+wet tuesday night in stoke
+
+real roy of the rovers stuff
+the supporters are in dream land
+squeaky bum time
+provincial club
+game of two halves
+
+Well jeff
+
+'what a goal that would have been'.
+    there or thereabouts
+int the mixer
+top top player
+parked the bus
+like a cup final
